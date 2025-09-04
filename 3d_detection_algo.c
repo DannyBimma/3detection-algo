11f37e55247e3d345d2b5e274ee2b3bd0d4e0244
DannyBimma <dantrotman92@gmail.com>
2025-09-03 20:23:54 -0400

    Include libs and macros.

--- 3d_detection_algo.c
+++ 3d_detection_algo.c
@@ -6,3 +6,13 @@
  * them into the appropriate joints (finger, hole, slot) for CAD/CAM
  * applications... I suppose
  */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define EPSILON 1e-9
+#define MAX_COMPONENTS 1000
+#define MAX_SEGMENTS 100
+#define MAX_JOINTS 500
