13c2ecb5d5498f7e2250af855b9957f2b44698cf
DannyBimma <dantrotman92@gmail.com>
2025-09-03 20:48:59 -0400

    Define the structures.

--- 3d_detection_algo.c
+++ 3d_detection_algo.c
@@ -16,3 +16,62 @@
 #define MAX_COMPONENTS 1000
 #define MAX_SEGMENTS 100
 #define MAX_JOINTS 500
+
+/* 3D Vector */
+typedef struct {
+  double x, y, z;
+} Vector3D;
+
+/* 3D Transformation Matrix (4x4 homogeneous) */
+typedef struct {
+  double m[4][4];
+} Matrix4x4;
+
+/* Line segment in 3D space */
+typedef struct {
+  Vector3D start;
+  Vector3D end;
+} Segment3D;
+
+/* Dynamic array for segments */
+typedef struct {
+  Segment3D *data;
+  int count;
+  int capacity;
+} SegmentArray;
+
+/* Types of joint... !marijuana */
+typedef enum { FINGER_JOINT, HOLE_JOINT, SLOT_JOINT } JointType;
+
+/* Joints */
+typedef struct {
+  JointType type;
+  Segment3D segment;
+} Joint;
+
+/* Dynamic array for joints */
+typedef struct {
+  Joint *data;
+  int count;
+  int capacity;
+} JointArray;
+
+/* 3D Component */
+typedef struct {
+  int id;
+  Vector3D *vertices;
+  int vertex_count;
+  Matrix4x4 transform_3d;
+  Matrix4x4 inverse_transform;
+  Vector3D normal;
+  JointArray fingers;
+  JointArray holes;
+  JointArray slots;
+} Component3D;
+
+/* Component collection */
+typedef struct {
+  Component3D *components;
+  int count;
+  int capacity;
+} ComponentArray;
