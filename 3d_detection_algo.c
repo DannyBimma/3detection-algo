c3b8afa9c884b24cb968fe2c3f01dc02a4068f69
DannyBimma <dantrotman92@gmail.com>
2025-09-03 20:21:05 -0400

    Init file.

---
+++ 3d_detection_algo.c
@@ -0,0 +1,8 @@
+/*
+ * 3D Component Intersection Detection and Joint Classification Algorithm
+ * My ANSI-C Implementation
+ *
+ * This algorithm detects intersections between 3D components and classifies
+ * them into the appropriate joints (finger, hole, slot) for CAD/CAM
+ * applications... I suppose
+ */
